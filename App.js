d7bfe2c1274ae7ebdd184e563f3cc3f9314c1b89
Gyuri21 <gyuri21hegedus@gmail.com>
2021-11-22 10:47:36 +0100

    kesz

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import React,{useState} from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import {Text, View,TouchableHighlight, } from 'react-native';
+import styles from './app.style.js';
 
 export default function App() {
-  const {all,setAll} = useState(0);
+  const [all,setAll] = useState(0);
+  const [car,setCar] = useState(0);
+  const[truck,setTruck] = useState(0);
+  function handleDelButton(){
+    setAll(0);
+    setCar(0);
+    setTruck(0);
+  }
+  function addCarBtn(){
+    setCar(car+1)
+  }
+  function addTruckBtn(){
+    setTruck(truck+1)
+  }
+  function setAllBtn(){
+    setAll(car+truck)
+  }
   return (
     <View style={styles.container}>
       <Text>Forgalom számláló</Text>
@@ -13,16 +30,33 @@ export default function App() {
         <Text style={styles.runText}>Mindent töröl</Text>
       </TouchableHighlight>
 
-      <Text>Összes: {all}</Text>
+      <Text>{car}</Text>
+
+      <TouchableHighlight 
+          style={styles.runButton} 
+          onPress={addCarBtn}>
+        <Text style={styles.runText}>Személy</Text>
+      </TouchableHighlight>
+      
+
+      <Text>{truck}</Text>
+
+      <TouchableHighlight 
+          style={styles.runButton} 
+          onPress={addTruckBtn}>
+        <Text style={styles.runText}>Teher</Text>
+      </TouchableHighlight>
+
+      <Text>{all}</Text>
+
+      <TouchableHighlight 
+          style={styles.runButton} 
+          onPress={setAllBtn}>
+        <Text style={styles.runText}>Összes</Text>
+      </TouchableHighlight>
+
     </View>
+    
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ app.style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native-web";
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    runButton: {
+      backgroundColor: 'blue',
+      marginTop: 10,
+      marginBottom: 70,
+      padding: 10,
+      borderRadius: 3,
+  
+    },
+    runText: {
+      color: 'white',
+      paddingLeft: 10,
+      paddingRight: 10,
+      fontSize: 22,
+    },
+    sumtext:{
+      marginBottom: 70,
+    },
+  
+  });
